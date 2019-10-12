7ff94b393136ba416ae8b6e6abf470e491a5a62a
rcb-ferreira <ferreira.rcb@gmail.com>
2019-10-12 12:46:36 +0200

    refactor(cakes): add service to call local end point and list them

---
+++ src/app/cakes/api.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Observable, of, throwError } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { catchError, tap, map } from 'rxjs/operators';
+import { Cake } from './cake';
+
+const apiUrl = 'http://localhost:4000/api/v1/cakes';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(readonly http: HttpClient) {}
+
+  getCakes(): Observable<Cake[]> {
+    return this.http.get<Cake[]>(apiUrl, httpOptions);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+}
--- src/app/cakes/cakes.component.ts
+++ src/app/cakes/cakes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+// import { ApiService } from '../api.service';
+import { Cake } from './cake';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-cakes',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cakes.component.scss']
 })
 export class CakesComponent implements OnInit {
+  data: Cake[] = [];
 
-  constructor() { }
+  constructor(private api: ApiService) { }
 
   ngOnInit() {
+
+    this.api.getCakes().subscribe(data => this.data = data);
   }
+
+  deleteCake(id, index) {}
 
 }
