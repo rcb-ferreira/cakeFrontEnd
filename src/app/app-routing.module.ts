1aa75fca460159a69ad6f358032973cad9bc886e
rcb-ferreira <ferreira.rcb@gmail.com>
2019-10-12 12:24:06 +0200

    feat(cakes): generate cakes component folder structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { CakesComponent } from './cakes/cakes.component';
+import { AddComponent } from './cakes/add/add.component';
+import { EditComponent } from './cakes/edit/edit.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: CakesComponent,
+    data: { title: 'List of Cakes' }
+  },
+  {
+    path: 'cakes/add',
+    component: AddComponent,
+    data: { title: 'Add Cakes' }
+  },
+  {
+    path: 'cakes/edit/:id',
+    component: EditComponent,
+    data: { title: 'Edit cakes' }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CakesComponent } from './cakes/cakes.component';
+import { AddComponent } from './cakes/add/add.component';
+import { EditComponent } from './cakes/edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CakesComponent,
+    AddComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
